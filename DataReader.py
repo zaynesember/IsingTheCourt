57d37406da3484c546e0519c78159cb0dc54dc3b
zaynesember <zaynesem@buffalo.edu>
2020-04-17 14:08:53 -0400

    Created correlation heatmaps

--- DataReader.py
+++ DataReader.py
@@ -7,21 +7,23 @@ class DataReader:
     #Takes in existing array of votes for an individual case and direction of a vote and transforms then
     #appends it to array
     def directionCheck(caseVotes, direction):
-        #Conservative vote
+        #Conservative or dissenting vote
         if(direction == "1"): return np.append(caseVotes,-1.)
-        #Liberal vote
+        #Liberal or majority vote
         if(direction == "2"): return np.append(caseVotes,1.)
         #No direction
-        else: return np.append(caseVotes,0.)
+        else: return np.append(caseVotes,0)
         
     #Scrapes vote data for the Rehnquist Court from 1994 to 2005
-    def getRehnquist():
+    def getRehnquist(ideology=False, mm=False):
+        if ideology: rowidx=57
+        if mm: rowidx=58
         
         allVotes = np.array([None,None,None,None,None,None,None,None,None])
         #allVotes = np.empty(shape=(9,1))
         caseVotes = np.array([])
         
-        with open('SCDB_2019_01_justiceCentered_Citation_utf.csv') as csv_file:
+        with open('SCDB_2019_01_justiceCentered_Citation_utf.csv', encoding='utf-8') as csv_file:
             
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
@@ -47,18 +49,20 @@ class DataReader:
                     
                         if(prevCaseID == caseIDint or prevCaseID == -1):
                             
-                            #Note this is using the 'direction' column where votes are categorized by ideology, other measures may be better
-                            direction = row[57]
+                            direction = row[rowidx]
                             #Append the transformed vote direction to the array
                             caseVotes = DataReader.directionCheck(caseVotes, direction)
 
                         else: 
                             #Check for oddball cases where not all justices voted and ignore them
                             if(caseVotes.size == 9):
-                                allVotes = np.concatenate([allVotes.reshape(-1,9), caseVotes.reshape(-1,9)],axis=0)
+                                zeroes = False
+                                for v in caseVotes:
+                                    if v==0: zeroes = True
+                                if (not zeroes): allVotes = np.concatenate([allVotes.reshape(-1,9), caseVotes.reshape(-1,9)],axis=0)
                                 caseVotes = np.array([])
                             
-                            direction = row[57]
+                            direction = row[rowidx]
                             caseVotes = DataReader.directionCheck(caseVotes, direction)
 
                         prevCaseID = caseIDint
